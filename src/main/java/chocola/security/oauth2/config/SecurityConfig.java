b9e001820a1598c6954f88daa102fa73d7bf40df
chocolaggibbiddori <didrkd684@naver.com>
2025-03-08 00:52:36 +0900

    [Section 2] Spring Security Fundamentals - AuthenticationEntryPoint 이해

--- src/main/java/chocola/security/oauth2/config/SecurityConfig.java
+++ src/main/java/chocola/security/oauth2/config/SecurityConfig.java
@@ -2,29 +2,24 @@ package chocola.security.oauth2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain1(HttpSecurity httpSecurity) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .authorizeHttpRequests(request -> request.anyRequest().authenticated())
-                .formLogin(withDefaults())
-                .apply(new CustomSecurityConfigurer().setSecure(false));
+                .httpBasic(Customizer.withDefaults())
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint((request, response, authException) ->
+                                System.out.println("custom entrypoint")
+                        )
+                );
 
         return httpSecurity.build();
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain2(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
-                .httpBasic(withDefaults())
-                .build();
     }
 }
