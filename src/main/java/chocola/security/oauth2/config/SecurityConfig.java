908132857cd73a5e098d444ab7ae47cccd20076c
chocolaggibbiddori <didrkd684@naver.com>
2025-03-17 21:28:09 +0900

    [Section 2] Spring Security Fundamentals - Http Basic 인증

--- src/main/java/chocola/security/oauth2/config/SecurityConfig.java
+++ src/main/java/chocola/security/oauth2/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package chocola.security.oauth2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,15 +10,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
+        return httpSecurity
                 .authorizeHttpRequests(request -> request.anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
-                .exceptionHandling(exception -> exception
-                        .authenticationEntryPoint((request, response, authException) ->
-                                System.out.println("custom entrypoint")
-                        )
-                );
-
-        return httpSecurity.build();
+                .httpBasic(basic -> basic
+                        .authenticationEntryPoint(new CustomAuthenticationEntryPoint()))
+                .build();
     }
 }
