7779e41f4ca4260b8cbd635a7a83a2f78fdbf6c5
chocolaggibbiddori <didrkd684@naver.com>
2025-03-17 22:19:02 +0900

    [Section 2] Spring Security Fundamentals - Cors

--- src/main/java/chocola/security/oauth2/config/SecurityConfig.java
+++ src/main/java/chocola/security/oauth2/config/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -12,8 +15,21 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .authorizeHttpRequests(request -> request.anyRequest().authenticated())
-                .httpBasic(basic -> basic
-                        .authenticationEntryPoint(new CustomAuthenticationEntryPoint()))
+                .cors(cors -> cors
+                        .configurationSource(corsConfigurationSource()))
                 .build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
     }
 }
