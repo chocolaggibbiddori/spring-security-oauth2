0ee7fd35425c346975f94e8c1ce7ef5ee5d2ca25
chocolaggibbiddori <didrkd684@naver.com>
2025-04-03 00:09:00 +0900

    [Section 7] OAuth 2.0 Client Fundamentals - ClientRegistrationRepository 이해 및 활용

--- src/main/java/chocola/security/oauth2/config/SecurityConfig.java
+++ src/main/java/chocola/security/oauth2/config/SecurityConfig.java
@@ -3,6 +3,10 @@ package chocola.security.oauth2.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ClientRegistrations;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,6 +15,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
+            .build();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(keycloakClientRegistration());
+    }
+
+    private ClientRegistration keycloakClientRegistration() {
+        return ClientRegistrations
+            .fromIssuerLocation("http://localhost:8080/realms/OAuth2")
+            .registrationId("keycloak")
+            .clientId("oauth2-client-app")
+            .clientSecret("PJrMtfY8Yv1SWyKCGgBXtFFzRvh1pTHU")
+            .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
             .build();
     }
 }
