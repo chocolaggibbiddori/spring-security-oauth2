f227ea0a121f16a1dc8abf706a640f2b7bb631ed
chocolaggibbiddori <didrkd684@naver.com>
2025-04-02 23:57:25 +0900

    [Section 7] OAuth 2.0 Client Fundamentals - 클라이언트 앱 시작하기

--- src/main/java/chocola/security/oauth2/config/SecurityConfig.java
+++ src/main/java/chocola/security/oauth2/config/SecurityConfig.java
@@ -4,9 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -14,22 +11,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
-                .cors(cors -> cors
-                        .configurationSource(corsConfigurationSource()))
-                .build();
-    }
-
-    private CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.addAllowedMethod("*");
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfiguration);
-
-        return source;
+            .build();
     }
 }
