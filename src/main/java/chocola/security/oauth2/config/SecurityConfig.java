b42c88db2a5383f19ee6ffe771a33ebb259b1959
chocolaggibbiddori <didrkd684@naver.com>
2025-04-04 21:58:09 +0900

    [Section 8] OAuth 2.0 Client oauth2Login() - OAuth2 로그인 구현 - OAuth 2.0 Login Page 생성

--- src/main/java/chocola/security/oauth2/config/SecurityConfig.java
+++ src/main/java/chocola/security/oauth2/config/SecurityConfig.java
@@ -3,11 +3,9 @@ package chocola.security.oauth2.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.ClientRegistrations;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
@@ -15,21 +13,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-            .build();
-    }
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository(keycloakClientRegistration());
-    }
-
-    private ClientRegistration keycloakClientRegistration() {
-        return ClientRegistrations
-            .fromIssuerLocation("http://localhost:8080/realms/OAuth2")
-            .registrationId("keycloak")
-            .clientId("oauth2-client-app")
-            .clientSecret("PJrMtfY8Yv1SWyKCGgBXtFFzRvh1pTHU")
-            .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
-            .build();
+                .authorizeHttpRequests(request -> request
+                        .anyRequest().authenticated())
+                .oauth2Login(withDefaults())
+                .build();
     }
 }
--- src/main/java/chocola/security/oauth2/controller/IndexController.java
+++ src/main/java/chocola/security/oauth2/controller/IndexController.java
@@ -1,25 +1,28 @@
 package chocola.security.oauth2.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 public class IndexController {
 
     private final ClientRegistrationRepository clientRegistrationRepository;
 
-    @Autowired
-    public IndexController(ClientRegistrationRepository clientRegistrationRepository) {
-        this.clientRegistrationRepository = clientRegistrationRepository;
-    }
-
     @GetMapping("/")
     public String index() {
         ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak");
-        System.out.println("clientRegistration = " + clientRegistration);
+        log.info("clientRegistration = {}", clientRegistration);
         return "index";
+    }
+
+    @GetMapping("/login-page")
+    public String loginPage() {
+        return "Login page";
     }
 }
